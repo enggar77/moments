7474c2e71c6f0f93be8dfe4eab9154f6ece12d3a
Purnama S Rahayu <purnamasrahayu@gmail.com>
2025-03-06 17:29:17 +0700

    [FIX] - fix error console Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -34,9 +34,9 @@ export default function Footer() {
 								viewBox='0 0 24 24'
 								fill='none'
 								stroke='currentColor'
-								stroke-width='2'
-								stroke-linecap='round'
-								stroke-linejoin='round'
+								strokeWidth='2'
+								strokeLinecap='round'
+								strokeLinejoin='round'
 							>
 								<path d='M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z' />
 							</svg>
@@ -49,9 +49,9 @@ export default function Footer() {
 								viewBox='0 0 24 24'
 								fill='none'
 								stroke='currentColor'
-								stroke-width='2'
-								stroke-linecap='round'
-								stroke-linejoin='round'
+								strokeWidth='2'
+								strokeLinecap='round'
+								strokeLinejoin='round'
 							>
 								<path d='M17 3H21L14 10L21 21H15L10 14L4 21H3L10 12L3 3H9L14 10L17 3Z' />
 							</svg>
@@ -64,9 +64,9 @@ export default function Footer() {
 								viewBox='0 0 24 24'
 								fill='none'
 								stroke='currentColor'
-								stroke-width='2'
-								stroke-linecap='round'
-								stroke-linejoin='round'
+								strokeWidth='2'
+								strokeLinecap='round'
+								strokeLinejoin='round'
 							>
 								<rect
 									width='20'
