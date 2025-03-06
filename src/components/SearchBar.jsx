28275c06f0b79db27c1d21fb83e7296266165148
Purnama S Rahayu <purnamasrahayu@gmail.com>
2025-03-06 17:24:34 +0700

    [FEATURE] - set filter SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,16 @@
 import { Form } from 'react-router';
-import Button from './Button';
 import { Search } from 'lucide-react';
+import { useEffect, useState } from 'react';
+import useDebounce from '../hooks/useDebounce';
 
-export default function SearchBar() {
+export default function SearchBar({ setSearchTerm }) {
+	const [inputValue, setInputValue] = useState('');
+	const debouncedValue = useDebounce(inputValue, 500);
+
+	useEffect(() => {
+		setSearchTerm(debouncedValue);
+	}, [debouncedValue, setSearchTerm]);
+
 	return (
 		<Form action="/search" className="relative w-full md:w-auto">
 			<input
@@ -10,6 +18,8 @@ export default function SearchBar() {
 				placeholder="Search for events..."
 				className="input  w-full lg:w-96 pl-10"
 				name="q"
+				value={inputValue}
+				onChange={(e) => setInputValue(e.target.value)}
 			/>
 			<Search className="text-base-content/30 absolute left-3 top-2 w-4" />
 		</Form>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,18 @@
+import { useState } from 'react';
 import EventList from '../components/EventList';
 import Hero from '../components/Hero';
 import SearchBar from '../components/SearchBar';
 
 export default function Home() {
+	const [searchTerm, setSearchTerm] = useState('');
 	return (
 		<div>
 			<Hero />
 			<section className="pt-[75vh]">
 				<div className="flex justify-end px-4 md:px-10 mb-5">
-					<SearchBar />
+					<SearchBar setSearchTerm={setSearchTerm} />
 				</div>
-				<EventList />
+				<EventList searchTerm={searchTerm} />
 			</section>
 		</div>
 	);
