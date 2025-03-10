6eb7992dc04435b3e2ebb20b8806e1979c756dee
Purnama S Rahayu <purnamasrahayu@gmail.com>
2025-03-11 00:36:04 +0700

    [FIX] restore Layout window scroll

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,17 @@
-import { Outlet } from 'react-router';
+import { Outlet, useLocation } from 'react-router';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import { useEffect } from 'react';
 
 export default function Layout() {
+	const location = useLocation();
+
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, [location]);
 	return (
 		<div className="bg-base-200 text-base-content flex flex-col">
-			<Navbar className="z-50"/>
+			<Navbar className="z-50" />
 			<main className=" flex-1">
 				<Outlet />
 			</main>
